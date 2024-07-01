e07c183c8a51e75927ed28c535a83f5e614790a4
DanLucas1 <dan.r.lucas@gmail.com>
2024-07-01 10:24:38 +0000

    testing workflow tf tear down

--- data_loaders/save_csv_to_gcs.py
+++ data_loaders/save_csv_to_gcs.py
@@ -1,21 +1,32 @@
 import io
-import pandas as pd
+# import pandas as pd
+import os
 import requests
+import zipfile
+from google.cloud import storage
+
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
+year = 2024
+quarter = 1
 
 @data_loader
 def load_data_from_api(*args, **kwargs):
     """
     Reading data from Indego CSV
     """
-    url = 'https://www.rideindego.com/wp-content/uploads/2024/04/indego-trips-2024-q1.zip'
-    response = requests.get(url)
+ 
+    headers = {'User-Agent':'student-application'}
+    
+    url = f'https://www.rideindego.com/wp-content/uploads/2024/04/indego-trips-{year}-q{quarter}.zip' 
+ 
+    response = requests.get(url, headers=headers)    
+    return response.status_code
 
-    return pd.read_csv(io.StringIO(response.text), sep=',')
+    # return pd.read_csv(io.StringIO(response.text), sep=',', nrows = 100, compression='zip')
 
 
 @test
